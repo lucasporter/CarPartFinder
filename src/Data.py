1aa8030a0f6d15d8ff102e4bc644252852a86862
lucasporter <Lucassporter@outlook.com>
2023-02-25 19:35:07 -0700

    changes to data

--- src/Data.py
+++ src/Data.py
@@ -1,11 +1,4 @@
 import csv
-def getVehicleInfo():
-    vehicleYear = input("Please enter your vehicle Year: ")
-    vehicleMake = input("Please enter your vehicle Make: ")
-    vehicleModel = input("Please enter your vehicle Model: ")
-    return vehicleYear, vehicleMake, vehicleModel
-def getPartInfo():
-    requestedPart = input("What part are you looking for today: ")
 
 def getCarData():
     with open('CarData/1926-2023.txt', 'r') as file_obj:
